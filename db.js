093b7dbfeb1fca7a01b4a11575ed63a70805527c
ramyacoorg <ramyascoorg@gmail.com>
2026-02-28 17:23:29 +0530

    fix ssl for local and render

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: isProduction ? process.env.DATABASE_URL : undefined,
+  user: isProduction ? undefined : process.env.DB_USER,
+  host: isProduction ? undefined : process.env.DB_HOST,
+  database: isProduction ? undefined : process.env.DB_NAME,
+  password: isProduction ? undefined : process.env.DB_PASSWORD,
+  port: isProduction ? undefined : process.env.DB_PORT,
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
